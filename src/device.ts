5044269a687d740f9b855709d5616f9068c2c024
Dídac García <didaquis@gmail.com>
2021-09-24 23:52:12 +0200

    Improved device configuration

--- src/device.ts
+++ src/device.ts
@@ -8,6 +8,7 @@ export interface MyDevice {
 interface SignalsOfDevice {
 	description: string;
 	signal: string;
+	requiresManualAction: boolean;
 }
 
 /**
@@ -21,66 +22,82 @@ export const MyDevice: MyDevice = {
 		{
 			description: 'Established communication',
 			signal: 'AQDwf39/fw==',
+			requiresManualAction: false,
 		},
 		{
 			description: 'Standby status',
 			signal: 'AgDwf39/fw==',
+			requiresManualAction: false,
 		},
 		{
 			description: 'Square button',
 			signal: 'AQjwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Triangle button',
 			signal: 'AQHwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Circle button',
 			signal: 'AQLwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Crux button',
 			signal: 'AQTwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'R1 button',
 			signal: 'ASDwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'R2 button',
 			signal: 'AYDwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'L1 button',
 			signal: 'ARDwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'L2 button',
 			signal: 'AUDwf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Up button',
 			signal: 'AQDwfwB/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Right button',
 			signal: 'AQDw/39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Down button',
 			signal: 'AQDwf/9/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Left button',
 			signal: 'AQDwAH9/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Select button',
 			signal: 'AQDyf39/fw==',
+			requiresManualAction: true,
 		},
 		{
 			description: 'Start button',
 			signal: 'AQDxf39/fw==',
+			requiresManualAction: true,
 		},
 	],
 };
