a53aeb933f09e5cbc81a6a5fa11be79858b01163
Dídac García <didaquis@gmail.com>
2021-09-25 02:07:53 +0200

    Added logic to log the actions of user

--- src/deviceConfigurator/monitorDevice.ts
+++ src/deviceConfigurator/monitorDevice.ts
@@ -19,83 +19,83 @@ export const monitorDevice = (): void => {
 
 	const device = usb.findByIds(TARGET_DEVICE.vendorId, TARGET_DEVICE.productId);
 
-	if (device !== undefined) {
-		try {
-			// Close the communication with the device.
-			// This situation may occur if the last run ended without closing the device properly.
-			device.close();
-		} catch {
-			// An error may occur if we try to close the communication with a device and the communication is not opened.
-			// We don't need to handle this error.
-		}
-		device.open();
-		//const allInterfaces = device.interfaces;
-		/**
-		[
-			Interface {
-				// ...
-			}
-		]
-		 */
-
-		const firstInterface = device.interface(0);
-
-		if (firstInterface && firstInterface.isKernelDriverActive()) {
-			firstInterface.detachKernelDriver();
-		}
-		firstInterface.claim();
-
-		//const allEndpoints = firstInterface.endpoints;
-		/*
-		[
-			InEndpoint {
-				// ...
-			}
-		]
-		*/
-
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const inEndpoint: any = firstInterface.endpoint(129); /* an "IN endpoint" means "device to PC" */
-
-		if (inEndpoint.direction === 'in') {
-			inEndpoint.startPoll();
-			inEndpoint.on('data', (buffer: Buffer) => {
-				const signal = buffer.toString('base64');
-
-				if (!listOfSignalsDetected.includes(signal)) {
-					listOfSignalsDetected.push(signal);
-					console.log(`signal detected: ${signal}`);
-					//console.log(`signal hex: ${buffer.toString('hex')}`); // An alternative method to represent the signal
-				}
-			});
-			inEndpoint.on('error', (error: unknown) => {
-				/*
-				enum libusb_transfer_status {
-					LIBUSB_TRANSFER_COMPLETED = 'Transfer completed without error. Note that this does not indicate that the entire amount of requested data was transferred.',
-					LIBUSB_TRANSFER_ERROR = 'Transfer failed.',
-					LIBUSB_TRANSFER_TIMED_OUT = 'Transfer timed out.',
-					LIBUSB_TRANSFER_CANCELLED = 'Transfer was cancelled.',
-					LIBUSB_TRANSFER_STALL = 'For bulk/interrupt endpoints: halt condition detected (endpoint stalled). For control endpoints: control request not supported.',
-					LIBUSB_TRANSFER_NO_DEVICE = 'Device was disconnected.',
-					LIBUSB_TRANSFER_OVERFLOW = 'Device sent more data than requested.',
-				}
-				*/
-				console.log('error event!', error);
-			});
-		}
-		console.log('\nRunning for 20 seconds. Please press all buttons on your device one at a time\n');
-
-		const twentySeconds = 20000;
-		setTimeout(function (): void {
-			console.log('\nlistOfSignalsDetected :\n', listOfSignalsDetected);
-
-			firstInterface.release(true, () => {
-				device.close();
-				process.exit();
-			});
-		}, twentySeconds);
-	} else {
+	if (device === undefined) {
 		console.log('Device not detected!');
 		process.exit();
 	}
+
+	try {
+		// Close the communication with the device.
+		// This situation may occur if the last run ended without closing the device properly.
+		device.close();
+	} catch {
+		// An error may occur if we try to close the communication with a device and the communication is not opened.
+		// We don't need to handle this error.
+	}
+	device.open();
+	//const allInterfaces = device.interfaces;
+	/**
+	[
+		Interface {
+			// ...
+		}
+	]
+		*/
+
+	const firstInterface = device.interface(0);
+
+	if (firstInterface && firstInterface.isKernelDriverActive()) {
+		firstInterface.detachKernelDriver();
+	}
+	firstInterface.claim();
+
+	//const allEndpoints = firstInterface.endpoints;
+	/*
+	[
+		InEndpoint {
+			// ...
+		}
+	]
+	*/
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const inEndpoint: any = firstInterface.endpoint(129); /* an "IN endpoint" means "device to PC" */
+
+	if (inEndpoint.direction === 'in') {
+		inEndpoint.startPoll();
+		inEndpoint.on('data', (buffer: Buffer) => {
+			const signal = buffer.toString('base64');
+
+			if (!listOfSignalsDetected.includes(signal)) {
+				listOfSignalsDetected.push(signal);
+				console.log(`signal detected: ${signal}`);
+				//console.log(`signal hex: ${buffer.toString('hex')}`); // An alternative method to represent the signal
+			}
+		});
+		inEndpoint.on('error', (error: unknown) => {
+			/*
+			enum libusb_transfer_status {
+				LIBUSB_TRANSFER_COMPLETED = 'Transfer completed without error. Note that this does not indicate that the entire amount of requested data was transferred.',
+				LIBUSB_TRANSFER_ERROR = 'Transfer failed.',
+				LIBUSB_TRANSFER_TIMED_OUT = 'Transfer timed out.',
+				LIBUSB_TRANSFER_CANCELLED = 'Transfer was cancelled.',
+				LIBUSB_TRANSFER_STALL = 'For bulk/interrupt endpoints: halt condition detected (endpoint stalled). For control endpoints: control request not supported.',
+				LIBUSB_TRANSFER_NO_DEVICE = 'Device was disconnected.',
+				LIBUSB_TRANSFER_OVERFLOW = 'Device sent more data than requested.',
+			}
+			*/
+			console.log('error event!', error);
+		});
+	}
+	console.log('\nRunning for 20 seconds. Please press all buttons on your device one at a time\n');
+
+	const twentySeconds = 20000;
+	setTimeout(function (): void {
+		console.log('\nlistOfSignalsDetected :\n', listOfSignalsDetected);
+
+		firstInterface.release(true, () => {
+			device.close();
+			process.exit();
+		});
+	}, twentySeconds);
 };
--- src/listenDevice.ts
+++ src/listenDevice.ts
@@ -4,5 +4,78 @@ import { USB_Device } from './types';
 export const listenDevice = (joypad: USB_Device): void => {
 	// eslint-disable-next-line no-console
 	console.log(`Listen the device: ${joypad.description}...`);
-	//const device = usb.findByIds(joypad.vendorId, joypad.productId);
+
+	const device = usb.findByIds(joypad.vendorId, joypad.productId);
+
+	if (device === undefined) {
+		// eslint-disable-next-line no-console
+		console.error('Device not detected! \n * Did you configure the device? \n * Is the device connected?');
+		process.exit();
+	}
+
+	try {
+		// Close the communication with the device.
+		// This situation may occur if the last run ended without closing the device properly.
+		device.close();
+	} catch {
+		// An error may occur if we try to close the communication with a device and the communication is not opened.
+		// We don't need to handle this error.
+	}
+	device.open();
+
+	const firstInterface = device.interface(0);
+
+	if (firstInterface && firstInterface.isKernelDriverActive()) {
+		firstInterface.detachKernelDriver();
+	}
+	firstInterface.claim();
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const inEndpoint: any = firstInterface.endpoint(129); /* an "IN endpoint" means "device to PC" */
+
+	const closeCommunicationWithTheDevice = (cb?: () => void) => {
+		try {
+			firstInterface.release(true, () => {
+				device.close();
+				if (cb) {
+					cb();
+				}
+			});
+		} catch {
+			if (cb) {
+				cb();
+			}
+		}
+	};
+
+	if (inEndpoint.direction === 'in') {
+		inEndpoint.startPoll();
+		inEndpoint.on('data', (buffer: Buffer): void => {
+			const signalDetected = buffer.toString('base64');
+
+			const signal = joypad.listOfSignals.find((signalToLog) => {
+				return signalToLog.signal === signalDetected && signalToLog.requireManualAction;
+			});
+
+			if (signal) {
+				// eslint-disable-next-line no-console
+				console.log(signal.description);
+			}
+		});
+		inEndpoint.on('error', (error: unknown): void => {
+			// eslint-disable-next-line no-console
+			console.log('error event!', error);
+			closeCommunicationWithTheDevice(() => {
+				process.exit();
+			});
+		});
+	}
+
+	process.on('SIGINT', () => {
+		// eslint-disable-next-line no-console
+		console.log('\n Stopping the application...');
+		closeCommunicationWithTheDevice(() => {
+			process.exit();
+		});
+	});
 };
